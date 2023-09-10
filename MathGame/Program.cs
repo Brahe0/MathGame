265221733bf9b65ca363d40220503e24b7bdc542
Brahe0 <bandreluis92@gmail.com>
2023-09-09 22:34:04 -0300

    types

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿Console.WriteLine("These are the most common data types:\n");
+Console.WriteLine("string" + " = André");
+Console.WriteLine("char = " + 'a');
+Console.WriteLine("int = " + 2);
+Console.WriteLine("decimal = " + 0.1m);
+Console.WriteLine("bool = " + true);
